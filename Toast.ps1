58b7ad67511e246667c882434e0c63ca3fcf99b8
SYMBIOTEADMIN <dibyasambitsahu@gmail.com>
2022-01-30 15:59:17 +0530

    Adding exe and ps1 files...

---
+++ Toast.ps1
@@ -0,0 +1,52 @@
+﻿
+function Show-Notification {
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "PowerShell"
+    $Toast.Group = "PowerShell"
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("Battery Notifier")
+    $Notifier.Show($Toast);
+}
+
+
+
+While(1)
+{
+
+    
+    $charge = Get-CimInstance -ClassName Win32_Battery | Select-Object -ExpandProperty EstimatedChargeRemaining
+    $threshold = 85
+    
+    if ( $charge -gt $threshold )
+    {
+        
+    
+        Show-Notification -ToastTitle "Unplug A/C Charger" -ToastText "The Battery % is $charge% above $threshold%"
+        Start-Sleep -s 300
+        continue
+
+        
+    }
+
+    Start-Sleep -s 300
+}
